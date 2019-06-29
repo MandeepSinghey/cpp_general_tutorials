f78459f7283c7bd0b670cc1c8ea30736c9c4f2bb
Mandeep Singh <Mandeep.singh@ey.com>
2019-06-28 21:56:19 -0400

    Added c plus plus primer

---
+++ c_plus_plus_primer/book_sales/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Sales_item.h"
+
+using namespace std;
+
+int main()
+{
+    Sales_item book1, book2;
+    //read input
+    std::cin >> book1;
+    std::cin >> book2;
+    //output
+    std::cout << book1 << std::endl;
+    std::cout << book2 << std::endl;
+    return 0;
+}
